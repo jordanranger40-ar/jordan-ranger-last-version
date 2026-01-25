4a15355872f2d20c424d35ef4a61997eda4f64c8
BelalAbandah <belalabandah@gmail.com>
2026-01-25 10:41:37 +0300

    fixing training page

--- components/corporate-team-building/CorporateTeamBuildingSection.tsx
+++ components/corporate-team-building/CorporateTeamBuildingSection.tsx
@@ -18,15 +18,26 @@ export default async function CorporateTeamBuildingSection({
       <h2 className="text-5xl font-bold mb-6 text-[#2c3e50]">
         {isArabic ? "بناء فرق الشركات" : "Corporate Team Building"}
       </h2>
-      <p className="text-center text-gray-600 mb-16 max-w-3xl">
+      <p className="text-center text-gray-600 mb-16 max-w-5xl">
         {isArabic
-          ? "استمتع بمجموعة متنوعة من أنشطة بناء فرق العمل للشركات."
-          : "Enjoy a variety of Corporate Team Building activities."}
+          ? `تُعدّ جوردن رينجر من الروّاد في مجال أنشطة بناء فرق العمل للشركات، حيث نأخذ المشاركين خارج منطقة راحتهم ونضعهم في بيئة جديدة ومحفّزة، تمتاز بالهواء النقي والمناظر الطبيعية الخلّابة، وبأجواء من الهدوء والسكينة بعيدًا عن مكاتب العمل المغلقة.
+
+نشجّع المشاركين على استكشاف كامل إمكاناتهم، وبناء ثقتهم بأنفسهم، وتعزيز علاقاتهم مع أعضاء الفريق الآخرين من خلال روح الزمالة والمتعة الحقيقية.
+
+ومن خلال تحدياتنا المصمّمة خصيصًا والتي تجمع بين التحديات البدنية والذهنية، ساهمت أنشطة بناء الفرق الخارجية التي نقدّمها في مساعدة مئات الشركات في الأردن والمنطقة على تحقيق أهدافها المنشودة.`
+          : `Jordan Ranger has been a pioneer in corporate team building activities, taking participants out of their comfort zone and placing them in a new, stimulating environment with fresh air, beautiful scenery and a sense of peace and quiet, away from confined office spaces.
+
+We encourage our participants to explore their full potential and build their confidence as well as their relationships with other team members through comradery and good old fun!
+
+Our customized challenges that include both body and mental challenges, our outdoor team building activities have helped hundreds of corporations from Jordan and the region achieve their set goals.`}
       </p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-6 w-full max-w-7xl">
         {data.data.map((data, idx) => (
-          <Link key={idx} href={`/training/corporate-team-building/${data.slug ?? ""}`}>
+          <Link
+            key={idx}
+            href={`/training/corporate-team-building/${data.slug ?? ""}`}
+          >
             <TrainingsCard isArabic={isArabic} data={data} />
           </Link>
         ))}
--- components/school-training/trainingSection.tsx
+++ components/school-training/trainingSection.tsx
@@ -13,18 +13,24 @@ export default async function TrainingSection({ isArabic }: Props) {
       dir={isArabic ? "rtl" : "ltr"}
       className="w-full min-h-screen py-24 bg-[#f4f6f8] flex flex-col items-center"
     >
-      <h2 className="text-5xl font-bold mb-6 text-[#2c3e50]">
+      <h2 className="text-2xl md:text-5xl font-bold mb-6 text-[#2c3e50]">
         {isArabic ? "التدريب المدرسي" : "School Training"}
       </h2>
-      <p className="text-center text-gray-600 mb-16 max-w-3xl">
+      <p className="text-center text-gray-600 mb-16 max-w-5xl">
         {isArabic
-          ? "استمتع بمجموعة متنوعة من التدريبات المدرسية."
-          : "Enjoy a variety of School Activities."}
+          ? `يقدّم جوردن رينجر تدريبًا مدرسيًا تفاعليًا صُمّم لتعزيز الأهداف التعليمية للمدارس، مثل العمل الجماعي، والقيادة، وتنمية مهارات حلّ المشكلات، والطهي في الهواء الطلق لدى الطلاب. يتضمن برنامجنا مجموعة متنوعة من العوائق والتحديات العسكرية المصمّمة بعناية لتعزيز روح التعاون وبناء القدرة على التحمّل.
+
+يُركّز تدريبنا على تطوير اللياقة البدنية، والمرونة الذهنية، والتفكير الاستراتيجي، بما يهيّئ المشاركين لمواجهة مواقف الحياة الواقعية المختلفة. انضموا إلينا لننطلق معًا في رحلة شيّقة من النمو والتعلّم والمغامرة!`
+          : `The school training offered by Jordan Ranger is an engaging program designed to enhance the educational objectives of the school, like teamwork, leadership, problem-solving skills, and outdoor cooking, among students. Our course features a variety of obstacles and Army challenges carefully crafted to promote cooperation and resilience.
+Our training is tailored to enhance physical fitness, mental agility, and strategic thinking, preparing individuals for various real-world situations. Join us as we embark on an exciting journey of growth, learning, and adventure together!`}
       </p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-6 w-full max-w-7xl">
         {data.data.map((data, idx) => (
-          <Link key={idx} href={`/training/schools-training/${data.slug ?? ""}`}>
+          <Link
+            key={idx}
+            href={`/training/schools-training/${data.slug ?? ""}`}
+          >
             <TrainingsCard isArabic={isArabic} data={data} />
           </Link>
         ))}
