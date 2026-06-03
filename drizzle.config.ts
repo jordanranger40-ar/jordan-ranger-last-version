46ea8431155c059496537b9a8dddaa4dd830b41b
BelalAbandah <belalabandah@gmail.com>
2026-06-03 17:07:57 +0300

    fixing drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./db/schema.ts",
+  schema: "./dbSchemas/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_CONNECTION_STRING!,
   },
 } satisfies Config;
+
+
+// Note that: i am using drizzle just for saving the database schemas, the quries are written in SQL
+// this path:  ./dbSchemas/schema.ts has the last updated schema, you can build the database using this command: 'npx drizzle-kit push', but make sure to add the db connection string in the .env
+// if you want to do any changes on database, do it from neon, not from drizzle, and after updating the database, use this command:'npx drizzle-kit pull' to pull the updated db schema
+
